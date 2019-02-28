a9ca7f7dc615730047eed97cb2be8a40eb341956
avinash2wards <avinash2wards@outlook.com>
2019-02-28 23:56:46 +0530

    Upvote and downvote to show basics of redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>De-mystifying Redux!</h1>`;
+var h1 = document.createElement("h1");
+h1.innerText = "De-mystifying Redux!";
+document.getElementById("app").prepend(h1);
+
+var countLabel = document.getElementById("countLabel");
+var Redux = window.Redux;
+
+// Event handlers
+
+document.getElementById("upvote").addEventListener("click", function() {
+	store.dispatch({ type: "INCREMENT" });
+});
+
+document.getElementById("downvote").addEventListener("click", function() {
+	store.dispatch({ type: "DECREMENT" });
+});
+
+function counter(state = { count: 0 }, action) {
+	switch (action.type) {
+		case "INCREMENT":
+			return { state, ...{ count: state.count + 1 } };
+		case "DECREMENT":
+			return { state, ...{ count: state.count - 1 } };
+		default:
+			return state;
+	}
+}
+
+var store = Redux.createStore(counter);
+countLabel.innerHTML = store.getState().count;
+
+store.subscribe(function() {
+	countLabel.innerHTML = store.getState().count;
+});
